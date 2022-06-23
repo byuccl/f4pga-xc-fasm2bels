f5f76fb05c9c169726441b03cc7d044cbc3c3ef7
Jake Edvenson <jakers.edvenson@gmail.com>
2022-06-23 06:18:58 -0600

    Testing CI vivado interactions

---
+++ .github/workflows/create_goldenFile.sh
@@ -0,0 +1,6 @@
+source /tools/Xilinx/Vivado/2019.2/settings64.sh 
+vivado -mode batch -source create_goldenFile.tcl
+rm -r *.dcp
+rm -r *.edf
+rm report_io.txt
+mv *_impl.v tests/equivalence_checking_data/add32/top_bit.golden.v
